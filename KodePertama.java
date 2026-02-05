4b97441155e0a6933d4002abde1731e1aadd22cb
Kiyui00 <visqyaassyura@gmail.com>
2026-02-05 14:36:44 +0700

    KodePertama

---
+++ KodePertama.java
@@ -0,0 +1,10 @@
+/*Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 5 Februari 2026 
+*/
+public class KodePertama{
+    public static void main(String[] args) {
+     System.out.println("Halo, Pemrograman Berorientasi Objek!");
+     System.out.println("Ini adalah program java pertama saya.");   
+    }
+}
