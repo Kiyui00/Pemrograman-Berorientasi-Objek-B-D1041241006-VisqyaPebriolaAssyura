049806f51a74e87eecb46ec7076f70b36679799f
Kiyui00 <visqyaassyura@gmail.com>
2026-02-12 14:06:49 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,15 @@
+/*Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 12 Februari 2026 
+*/
+package Pertemuan3;
+
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+    int angka = 10;
+    System.out.println("Nilai awal angka: " + angka);
+    angka = 25;
+    System.out.println("Nilai angka setelah diubah: " + angka);
+
+    }
+}
