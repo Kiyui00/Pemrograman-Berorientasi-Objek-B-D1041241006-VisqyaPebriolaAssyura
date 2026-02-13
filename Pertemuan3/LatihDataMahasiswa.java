4f4ac5068819a7debbb43e9957ffba586be11896
Kiyui00 <visqyaassyura@gmail.com>
2026-02-13 07:50:46 +0700

    Praktikum_Latihan3.1_DataMahasiswa

---
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,27 @@
+/*Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 13 Februari 2026 
+*/
+
+package Pertemuan3;
+public class LatihDataMahasiswa {
+    public static void main(String[] args) {
+
+    System.out.println("=== DATA MAHASISWA ===");
+    
+    long nim = 2301010001L;
+    String nama = "Budi Santoso";
+    char kelas = 'A';
+    boolean status_aktif = true;
+    double ipk = 3.75;
+    int sks = 72;
+    
+    System.out.println("NIM                 : " + nim);
+    System.out.println("Nama                : " + nama);
+    System.out.println("Kelas               : " + kelas);
+    System.out.println("Status Aktif        : " + status_aktif);
+    System.out.println("IPK Semester        : " + ipk);
+    System.out.println("Total SKS           : " + sks);
+    
+    }
+}
