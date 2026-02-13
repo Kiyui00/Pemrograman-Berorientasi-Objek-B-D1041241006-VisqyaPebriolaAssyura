1b0f2c792a89bd37d78fa1f7a7b2f820719cacee
Kiyui00 <visqyaassyura@gmail.com>
2026-02-13 08:26:23 +0700

    Mengerjakan contoh program Naming Convention

---
+++ Pertemuan4/p041NamingConventio.java
@@ -0,0 +1,19 @@
+/*Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 13 Februari 2026 
+*/
+
+package Pertemuan4;
+public class p041NamingConventio {
+    public static final double PI= 3.14;
+    public static void main(String[] args) {
+        int studentAge = 20;
+        String studentName = "Budi";
+        boolean isActive = true;
+
+        System.out.println("Nama                : " + studentName);
+        System.out.println("Umur                : " + studentAge);
+        System.out.println("Status              : " + isActive);
+        System.out.println("Konstanta PI        : " + PI);
+    }
+}
