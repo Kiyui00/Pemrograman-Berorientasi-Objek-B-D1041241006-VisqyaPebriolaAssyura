3f65eac8d29f7782f3ac4436c0cece1b4841ccd1
Kiyui00 <visqyaassyura@gmail.com>
2026-02-25 08:24:53 +0700

    Pertemuan 7 Latihan 3 (For Loop)

---
+++ Pertemuan7/LatihanTabelPerkalian.java
@@ -0,0 +1,17 @@
+/*Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 25 Februari 2026 
+*/
+package Pertemuan7;
+
+public class LatihanTabelPerkalian {
+    public static void main(String[] args) {
+        int angka = 7;
+
+        System.out.println("=== TABEL PERKALIAN 7 ===");
+       
+        for(int i = 1; i <= 12; i++) {
+            System.out.println(angka + " x " + i + " = " + angka * i);
+        }
+    }
+}
