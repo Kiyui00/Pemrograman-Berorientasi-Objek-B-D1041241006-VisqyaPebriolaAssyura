6da04b07a80491fb4c6fa7caf10144e2b7d004bc
Kiyui00 <visqyaassyura@gmail.com>
2026-03-12 09:25:50 +0700

    Latihan 1 : Latihan Buku Constructor

---
+++ Pertemuan10/LatihanBukuConstructor.java
@@ -0,0 +1,47 @@
+/*
+Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 11 Maret 2026 
+*/
+package Pertemuan10;
+
+class Buku {
+    String judul;
+    String pengarang;
+    int harga;
+
+    public Buku() {
+        judul = "Belum diisi";
+        pengarang = "Belum diisi";
+        harga = 0;
+    }
+
+    public Buku(String judul, String pengarang, int harga) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.harga = harga;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("=== INFORMASI BUKU ===");
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Harga : Rp " + harga);
+        System.out.println();
+        System.out.println();
+    }
+}
+
+public class LatihanBukuConstructor {
+    public static void main(String[] args) {
+
+        Buku buku1 = new Buku();
+
+        Buku buku2 = new Buku("Pemrograman Java", "Budi Raharjo", 125000);
+        Buku buku3 = new Buku("Algoritma dan Struktur Data", "Rinaldi Munir", 150000);
+
+        buku1.tampilkanInfo();
+        buku2.tampilkanInfo();
+        buku3.tampilkanInfo();
+    }
+}
