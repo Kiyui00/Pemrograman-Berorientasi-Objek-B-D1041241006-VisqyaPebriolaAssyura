5b4e6fa783aeb72d0465a6341c330e0213c21623
Kiyui00 <visqyaassyura@gmail.com>
2026-04-24 10:36:28 +0700

    Latihan 1 Array Nama

---
+++ Pertemuan13/ArrayNama.java
@@ -0,0 +1,19 @@
+/*
+Nama: Visqya Pebriola Assyura  
+NIM: D1041241006
+Tanggal: 27 Maret 2026 
+*/
+package Pertemuan13;
+
+public class ArrayNama {
+    public static void main(String[] args) {
+        
+        String[] nama = {"Andi", "Budi", "Citra", "Deni", "Eka"};
+
+        System.out.println("=== DAFTAR NAMA ===");
+        for(int i = 0; i < 5; i++){
+            System.out.println(i+1 + ". " + nama[i]);
+        }
+        System.out.println();
+    }
+}
